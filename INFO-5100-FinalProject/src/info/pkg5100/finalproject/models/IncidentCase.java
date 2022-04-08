c891808d6705d63940111b62e1d1ea925b1e839d
ankitakumari89 <kumari.ank@northeastren.edu>
2022-04-08 18:10:30 -0400

    Adding incident class and test rsources.

---
+++ INFO-5100-FinalProject/src/info/pkg5100/finalproject/models/IncidentCase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package info.pkg5100.finalproject.models;
+
+/**
+ *
+ * @author ankit
+ */
+public class IncidentCase {
+	
+}

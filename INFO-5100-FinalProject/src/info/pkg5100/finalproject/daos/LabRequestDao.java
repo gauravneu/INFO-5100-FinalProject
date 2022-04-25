53b7a52d39b42430497b8287acee6ec070f0fc4b
Ankita kumari <kumari.ank@northeastern.edu>
2022-04-24 23:09:21 -0400

    Added Lab model

---
+++ INFO-5100-FinalProject/src/info/pkg5100/finalproject/daos/LabRequestDao.java
@@ -0,0 +1,21 @@
+package info.pkg5100.finalproject.daos;
+
+import info.pkg5100.finalproject.models.Enterprise;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface LabRequestDao {
+    public int add(Enterprise enterprise)
+            throws SQLException;
+    public void delete(int id)
+            throws SQLException;
+    public Enterprise getEnterpriseById(int id)
+            throws SQLException;
+    public List<Enterprise> getEnterprises()
+            throws SQLException;
+    public void update(Enterprise enterprise)
+            throws SQLException;
+    public List<Enterprise> getEnterpriseByLocation(String location)
+            throws SQLException;
+}
---
+++ INFO-5100-FinalProject/src/info/pkg5100/finalproject/models/LabRequest.java
@@ -0,0 +1,76 @@
+package info.pkg5100.finalproject.models;
+
+public class LabRequest {
+    int id;
+    int patientId;
+    String requestStatus;
+    String testName;
+    String testResult;
+    int labId;
+
+    public LabRequest(int id, int patientId, String requestStatus, String testName, String testResult, int labId) {
+        this.id = id;
+        this.patientId = patientId;
+        this.requestStatus = requestStatus;
+        this.testName = testName;
+        this.testResult = testResult;
+        this.labId = labId;
+    }
+
+    public LabRequest() {
+        this.id = -1;
+        this.patientId = -1;
+        this.requestStatus = "";
+        this.testName = "";
+        this.testResult = "";
+        this.labId = -1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getRequestStatus() {
+        return requestStatus;
+    }
+
+    public void setRequestStatus(String requestStatus) {
+        this.requestStatus = requestStatus;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getTestResult() {
+        return testResult;
+    }
+
+    public void setTestResult(String testResult) {
+        this.testResult = testResult;
+    }
+
+    public int getLabId() {
+        return labId;
+    }
+
+    public void setLabId(int labId) {
+        this.labId = labId;
+    }
+}

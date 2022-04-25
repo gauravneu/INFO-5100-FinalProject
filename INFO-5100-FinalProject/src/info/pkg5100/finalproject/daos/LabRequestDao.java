6755aeb84f89870ea7216ee98a04bf3d6c98bbd1
Ankita kumari <kumari.ank@northeastern.edu>
2022-04-24 23:34:18 -0400

    Added LabRequestDao implementation

--- INFO-5100-FinalProject/src/info/pkg5100/finalproject/daos/LabRequestDao.java
+++ INFO-5100-FinalProject/src/info/pkg5100/finalproject/daos/LabRequestDao.java
@@ -1,21 +1,23 @@
 package info.pkg5100.finalproject.daos;
 
 import info.pkg5100.finalproject.models.Enterprise;
+import info.pkg5100.finalproject.models.LabRequest;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface LabRequestDao {
-    public int add(Enterprise enterprise)
+    public int add(LabRequest labRequest)
             throws SQLException;
     public void delete(int id)
             throws SQLException;
-    public Enterprise getEnterpriseById(int id)
+    public LabRequest getLabRequestById(int id)
             throws SQLException;
-    public List<Enterprise> getEnterprises()
+    public List<LabRequest> getLabRequests()
             throws SQLException;
-    public void update(Enterprise enterprise)
+    public List<LabRequest> getLabRequestsByPatientId(int patientId)
             throws SQLException;
-    public List<Enterprise> getEnterpriseByLocation(String location)
+    public void update(LabRequest labRequest)
             throws SQLException;
+
 }

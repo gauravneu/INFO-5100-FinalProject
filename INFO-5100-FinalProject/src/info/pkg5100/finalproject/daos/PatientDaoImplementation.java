7501069b71d8d66fc193ac60e0f4be28ebd8a0f4
gauravneu <99050945+gauravneu@users.noreply.github.com>
2022-04-26 02:30:00 -0400

    updated patientdaoimplementation

--- INFO-5100-FinalProject/src/info/pkg5100/finalproject/daos/PatientDaoImplementation.java
+++ INFO-5100-FinalProject/src/info/pkg5100/finalproject/daos/PatientDaoImplementation.java
@@ -17,8 +17,11 @@ public class PatientDaoImplementation implements PatientDao{
     @Override
     public int add(Patient patient) throws SQLException {
         String query
-                = "insert into patients(id, name, age, phone, issue, status, incidentcaseid, labrequested, pharmacyrequested, hospitalid, doctorid)"
-                + "VALUES (?, ?, ?, ?, ?,?,?,?,?,?,?)";
+                = "insert into patients(id, name, age, phone, issue, status, "
+                + "incidentcaseid, labrequested, pharmacyrequested, hospitalid, "
+                + "doctorid, location, isconvicted, housingperiod,  "
+                + "detentionperiod, investigationpoliceid, housingid , detentionid)"
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement ps
                 = con.prepareStatement(query);
         ps.setInt(1, patient.getId());
@@ -32,6 +35,13 @@ public class PatientDaoImplementation implements PatientDao{
         ps.setString(9, patient.getPharmacyRequested());
         ps.setInt(10, patient.getHospitalId());
         ps.setInt(11, patient.getDoctorId());
+        ps.setString(12, patient.getLocation());
+        ps.setString(13, patient.getIsConvicted());
+        ps.setInt(14, patient.getHousingPeriod());
+        ps.setInt(15, patient.getDetentionPeriod());
+        ps.setInt(16, patient.getInvestigationpoliceid());
+        ps.setInt(17, patient.getHousingid());
+        ps.setInt(18, patient.getDetentionid());
 
         int n = ps.executeUpdate();
         return n;
@@ -72,6 +82,13 @@ public class PatientDaoImplementation implements PatientDao{
             patient.setPharmacyRequested(rs.getString("pharmacyrequested"));
             patient.setHospitalId(rs.getInt("hospitalid"));
             patient.setDoctorId(rs.getInt("doctorid"));
+            patient.setLocation(rs.getString("location"));
+            patient.setIsConvicted(rs.getString("isconvicted"));
+            patient.setHousingPeriod(rs.getInt("housingperiod"));
+            patient.setDetentionPeriod(rs.getInt("detentionperiod"));
+            patient.setInvestigationpoliceid(rs.getInt("investigationpoliceid"));
+            patient.setHousingid(rs.getInt("housingid"));
+            patient.setDetentionid(rs.getInt("detentionid"));
         }
 
         if (check == true) {
@@ -102,6 +119,13 @@ public class PatientDaoImplementation implements PatientDao{
             patient.setPharmacyRequested(rs.getString("pharmacyrequested"));
             patient.setHospitalId(rs.getInt("hospitalid"));
             patient.setDoctorId(rs.getInt("doctorid"));
+            patient.setLocation(rs.getString("location"));
+            patient.setIsConvicted(rs.getString("isconvicted"));
+            patient.setHousingPeriod(rs.getInt("housingperiod"));
+            patient.setDetentionPeriod(rs.getInt("detentionperiod"));
+            patient.setInvestigationpoliceid(rs.getInt("investigationpoliceid"));
+            patient.setHousingid(rs.getInt("housingid"));
+            patient.setDetentionid(rs.getInt("detentionid"));
             ls.add(patient);
         }
         return ls;
@@ -130,6 +154,13 @@ public class PatientDaoImplementation implements PatientDao{
             patient.setPharmacyRequested(rs.getString("pharmacyrequested"));
             patient.setHospitalId(rs.getInt("hospitalid"));
             patient.setDoctorId(rs.getInt("doctorid"));
+            patient.setLocation(rs.getString("location"));
+            patient.setIsConvicted(rs.getString("isconvicted"));
+            patient.setHousingPeriod(rs.getInt("housingperiod"));
+            patient.setDetentionPeriod(rs.getInt("detentionperiod"));
+            patient.setInvestigationpoliceid(rs.getInt("investigationpoliceid"));
+            patient.setHousingid(rs.getInt("housingid"));
+            patient.setDetentionid(rs.getInt("detentionid"));
             ls.add(patient);
         }
         return ls;
@@ -158,6 +189,13 @@ public class PatientDaoImplementation implements PatientDao{
             patient.setPharmacyRequested(rs.getString("pharmacyrequested"));
             patient.setHospitalId(rs.getInt("hospitalid"));
             patient.setDoctorId(rs.getInt("doctorid"));
+            patient.setLocation(rs.getString("location"));
+            patient.setIsConvicted(rs.getString("isconvicted"));
+            patient.setHousingPeriod(rs.getInt("housingperiod"));
+            patient.setDetentionPeriod(rs.getInt("detentionperiod"));
+            patient.setInvestigationpoliceid(rs.getInt("investigationpoliceid"));
+            patient.setHousingid(rs.getInt("housingid"));
+            patient.setDetentionid(rs.getInt("detentionid"));
             ls.add(patient);
         }
         return ls;
@@ -187,6 +225,13 @@ public class PatientDaoImplementation implements PatientDao{
             patient.setPharmacyRequested(rs.getString("pharmacyrequested"));
             patient.setHospitalId(rs.getInt("hospitalid"));
             patient.setDoctorId(rs.getInt("doctorid"));
+            patient.setLocation(rs.getString("location"));
+            patient.setIsConvicted(rs.getString("isconvicted"));
+            patient.setHousingPeriod(rs.getInt("housingperiod"));
+            patient.setDetentionPeriod(rs.getInt("detentionperiod"));
+            patient.setInvestigationpoliceid(rs.getInt("investigationpoliceid"));
+            patient.setHousingid(rs.getInt("housingid"));
+            patient.setDetentionid(rs.getInt("detentionid"));
             ls.add(patient);
         }
         return ls;
@@ -195,7 +240,11 @@ public class PatientDaoImplementation implements PatientDao{
     @Override
     public void update(Patient patient) throws SQLException {
         String query
-                = "update patients set name=?, age=?, phone=?, issue=?, status=?, incidentcaseid=?, labrequested=? , pharmacyrequested=?, hospitalid=?, doctorid=? "
+                = "update patients set name=?, age=?, phone=?, issue=?, "
+                + "status=?, incidentcaseid=?, labrequested=? , "
+                + "pharmacyrequested=?, hospitalid=?, doctorid=? ,location=?, "
+                + "isconvicted=?, housingperiod=?,  detentionperiod=?,"
+                + " investigationpoliceid=?, housingid=?, detentionid=?"
                 + " where id = ?";
         PreparedStatement ps
                 = con.prepareStatement(query);
@@ -209,7 +258,51 @@ public class PatientDaoImplementation implements PatientDao{
         ps.setString(8, patient.getPharmacyRequested());
         ps.setInt(9, patient.getHospitalId());
         ps.setInt(10, patient.getDoctorId());
-        ps.setInt(11, patient.getId());
+        ps.setString(11, patient.getLocation());
+        ps.setString(12, patient.getIsConvicted());
+        ps.setInt(13, patient.getHousingPeriod());
+        ps.setInt(14, patient.getDetentionPeriod());
+        ps.setInt(15, patient.getInvestigationpoliceid());
+        ps.setInt(16, patient.getHousingid());
+        ps.setInt(17, patient.getDetentionid());
+        ps.setInt(18, patient.getId());
         ps.executeUpdate();
+    }
+
+    @Override
+    public List<Patient> getPatientsByLocationAndConvicted(String location, String isconvicted) throws SQLException {
+        String query = "select * from patients WHERE location=? and isconvicted =?";
+        PreparedStatement ps
+                = con.prepareStatement(query);
+
+        ps.setString(1, location);
+        ps.setString(2, isconvicted);
+        ResultSet rs = ps.executeQuery();
+        List<Patient> ls = new ArrayList();
+
+        while (rs.next()) {
+            Patient patient = new Patient();
+            patient.setId(rs.getInt("id"));
+            patient.setName(rs.getString("name"));
+            patient.setAge(rs.getString("age"));
+            patient.setPhoneNumber(rs.getString("phone"));
+            patient.setPatientIssue(rs.getString("issue"));
+            patient.setPatientStatus(rs.getString("status"));
+            patient.setIncidentCaseId(rs.getInt("incidentcaseid"));
+            patient.setLabRequested(rs.getString("labrequested"));
+            patient.setPharmacyRequested(rs.getString("pharmacyrequested"));
+            patient.setHospitalId(rs.getInt("hospitalid"));
+            patient.setDoctorId(rs.getInt("doctorid"));
+            patient.setLocation(rs.getString("location"));
+            patient.setIsConvicted(rs.getString("isconvicted"));
+            patient.setHousingPeriod(rs.getInt("housingperiod"));
+            patient.setDetentionPeriod(rs.getInt("detentionperiod"));
+            patient.setInvestigationpoliceid(rs.getInt("investigationpoliceid"));
+            patient.setHousingid(rs.getInt("housingid"));
+            patient.setDetentionid(rs.getInt("detentionid"));
+            ls.add(patient);
+        }
+        return ls;
+    
     }
 }
